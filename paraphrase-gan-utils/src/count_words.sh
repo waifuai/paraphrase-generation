6c467d3948b0eb19adb328db412833c2702769c3
thewaifuai <thewaifuai@users.noreply.github.com>
2025-02-15 12:23:09 +0000

    move to repos

--- paraphrase-gan-utils/src/count_words.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Counts the number of unique words in a file.
-
-echo "the full has number of unique words:";
-tr ' ' '\n' < parabank.tsv | sort | uniq -c | wc -l
--- paraphrase-gan-utils/src/multicore/phrase_generator/__init__.py
+++
@@ -1,2 +0,0 @@
-# This file can remain empty or can be used to expose
-# top-level package components if needed.
